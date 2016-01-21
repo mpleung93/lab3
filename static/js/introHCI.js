05f1e7df36a1ec8319733e8f8bc4f6b6f9fd323f
Marisa Leung <mpl005@ucsd.edu>
2016-01-21 11:42:55 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,57 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$('.jumbotron h1').text("Changing text of JUMBOTRON WOOO");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
+  $(".img").hide();
+
+	 $("#submitBtn").click(updateProject); 
+
+}
+
+function projectClick(e) {
+
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+    
+
+
+    var containingProject = $(this).closest(".project");
+
+    $(this).find('.img').toggle();
+
+
+    var description = $(containingProject).find(".project-description");
+                            console.log("Project clicked");
+
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+    	$(description).toggle();
+
+    }
+
+
+}
+
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
 }
